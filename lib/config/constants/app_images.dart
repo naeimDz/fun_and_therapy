070a5067e37a116f6c2d00f2f5eea6e7ba7c21db
naeimDz <naeimdz1703@gmail.com>
2023-04-15 04:00:35 +0100

    add themeData

---
+++ lib/config/constants/app_images.dart
@@ -0,0 +1,68 @@
+class AppImages {
+  /// Logo
+  static const String appLogo = '';
+  static const String appLogoDark = '';
+
+  static const String horizontalLogo = '';
+  static const String horizontalLogoDark = '';
+
+  /// Used in onboarding screens
+  static const String illustration1 = '';
+  static const String illustration2 = '';
+  static const String illustration3 = '';
+
+  // Other
+  static const String illustrationError = '';
+
+  static const illustrationSearch = '';
+
+  static const emptyPost = '';
+  static const emailSent = '';
+
+  static const notificationEmpty = '';
+
+  static const savedPostEmpty = '';
+
+  static const String noImageUrl = 'https://i.imgur.com/TxXtyVu.png';
+
+  static String defaultCategoryImage = '';
+
+  static const Map categoryThumbnails = {
+    // categoryID : 'category thumbnail url'
+    37: [
+      "واش ديري فطور #اليوم",
+      "https://img-global.cpcdn.com/recipes/3f5fdeacca7e680a/312x200cq70/photo.jpg",
+      "menu.json"
+    ],
+    5: [
+      "سهرة رمضانية",
+      "https://img-global.cpcdn.com/recipes/2948354f9ee08f36/312x200cq70/photo.jpg",
+      "sahra.json"
+    ],
+    16: [
+      "عصائر",
+      "https://img-global.cpcdn.com/recipes/5d3a7f511b743ba1/312x200cq70/photo.jpg",
+      "jus.json"
+    ],
+    38: [
+      "حفظ وتجميد",
+      "https://img-global.cpcdn.com/recipes/2a5e6a8de466b9a0/312x200cq70/photo.jpg",
+      "freez.json"
+    ],
+    1: [
+      "طواجن",
+      "https://img-global.cpcdn.com/recipes/5fefa32abc7a6729/312x200cq70/photo.jpg",
+      "tajin.json"
+    ],
+    3: [
+      "الشوربة",
+      "https://img-global.cpcdn.com/recipes/a4277dca208d1a9b/312x200cq70/photo.jpg",
+      "chorba.json"
+    ],
+    17: [
+      "حضريها في دارك",
+      "https://img-global.cpcdn.com/recipes/9e7db3440f78beee/312x200cq70/photo.jpg",
+      "athome.json"
+    ]
+  };
+}
