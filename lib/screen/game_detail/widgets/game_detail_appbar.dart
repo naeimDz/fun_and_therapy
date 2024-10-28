2c8962f9f6524c95bb03e4af37c22b41ee7ffb06
naeimDz <naeimdz1703@gmail.com>
2024-10-28 19:03:30 +0100

    ...

--- lib/screen/game_detail/widgets/game_detail_appbar.dart
+++ lib/screen/game_detail/widgets/game_detail_appbar.dart
@@ -1,25 +1,24 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-
-  AppBar detailsAppBar(BuildContext context) {
-    return AppBar(
-      backgroundColor: Colors.transparent,
-      elevation: 0,
-      leading: IconButton(
-        padding: EdgeInsets.only(right: 17),
-        icon: Icon(
-          Icons.arrow_back,
-          color: Colors.white,
-        ),
-        onPressed: () {
-          Navigator.pop(context);
-        },
+AppBar detailsAppBar(BuildContext context) {
+  return AppBar(
+    backgroundColor: Colors.transparent,
+    elevation: 0,
+    leading: IconButton(
+      padding: EdgeInsets.only(right: 17),
+      icon: Icon(
+        Icons.arrow_back,
+        color: Colors.white,
       ),
-      centerTitle: false,
-      title: Text(
-        'رجوع'.tr(),
-        style: Theme.of(context).textTheme.bodyText2,
-      ),
-    );
-  }
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    ),
+    centerTitle: false,
+    title: Text(
+      'رجوع'.tr(),
+      style: Theme.of(context).textTheme.bodyMedium,
+    ),
+  );
+}
